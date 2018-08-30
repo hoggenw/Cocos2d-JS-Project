a36ba23a2c034d345f2267233d09446bf32a8f45
hoggenw <253192463@qq.com>
2018-08-30 17:09:46 +0800

    主页开始

---
+++ HappyMiss/assets/Script/Controllers/GameController.js
@@ -0,0 +1,57 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+import GameModel from "../Model/GameModel";
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+        grid:{
+            default: null,
+            type: cc.Node
+        }
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // use this for initialization
+    onLoad: function () {
+        this.gameModel = new GameModel();
+        this.gameModel.init(5);
+        var gridScript = this.grid.getComponent("GridView");
+        gridScript.setController(this);
+        gridScript.initWithCellModels(this.gameModel.getCells());
+    },
+
+    selectCell: function(pos){
+        return this.gameModel.selectCell(pos);
+    },
+    cleanCmd: function(){
+        this.gameModel.cleanCmd();
+    }
+
+    // update (dt) {},
+});
---
+++ HappyMiss/assets/Script/Model/CellModel.js
@@ -0,0 +1,108 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+import { CELL_TYPE, ANITIME, CELL_STATUS, GRID_HEIGHT } from "./ConstValue";
+export default function CellModel(){
+    this.type = null;
+    this.status = CELL_STATUS.COMMON;
+    this.x = 1;
+    this.y = 1;
+    this.startX = 1;
+    this.startY = 1;
+    this.cmd = [];
+    this.isDeath = false;
+    this.objecCount = Math.floor(Math.random() * 1000);
+}
+CellModel.prototype.init= function(type){
+    this.type = type;
+}
+
+CellModel.prototype.isEmpty = function(){
+    return this.type == CELL_TYPE.EMPTY;
+}
+
+CellModel.prototype.setEmpty = function(){
+    this.type = CELL_TYPE.EMPTY;
+}
+CellModel.prototype.setXY = function(x,y){
+    this.x = x;
+    this.y = y;
+}
+
+CellModel.prototype.setStartXY = function(x,y){
+    this.startX = x;
+    this.startY = y;
+}
+
+CellModel.prototype.setStatus = function(status){
+    this.status = status;
+}
+
+CellModel.prototype.moveToAndBack = function(pos){
+    var srcPos = cc.p(this.x,this.y);
+    this.cmd.push({
+        action: "moveTo",
+        keepTime: ANITIME.TOUCH_MOVE,
+        playTime: 0,
+        pos: pos
+    });
+    this.cmd.push({
+        action: "moveTo",
+        keepTime: ANITIME.TOUCH_MOVE,
+        playTime: ANITIME.TOUCH_MOVE,
+        pos: srcPos
+    });
+}
+
+CellModel.prototype.moveTo = function(pos, playTime){
+    var srcPos = cc.p(this.x,this.y);
+    this.cmd.push({
+        action: "moveTo",
+        keepTime: ANITIME.TOUCH_MOVE,
+        playTime: playTime,
+        pos: pos
+    });
+    this.x = pos.x;
+    this.y = pos.y;
+}
+
+CellModel.prototype.toDie = function(playTime){
+    this.cmd.push({
+        action: "toDie",
+        playTime: playTime,
+        keepTime: ANITIME.DIE
+    });
+    this.isDeath = true;
+}
+
+CellModel.prototype.toShake = function(playTime){
+    this.cmd.push({
+        action: "toShake",
+        playTime: playTime,
+        keepTime: ANITIME.DIE_SHAKE
+    });
+}
+
+CellModel.prototype.setVisible = function(playTime, isVisible){
+    this.cmd.push({
+        action: "setVisible",
+        playTime: playTime,
+        keepTime: 0,
+        isVisible: isVisible
+    });
+}
+
+CellModel.prototype.moveToAndDie = function(pos){
+
+}
+
+CellModel.prototype.isBird = function(){
+    return this.type == CELL_TYPE.G;
+}
